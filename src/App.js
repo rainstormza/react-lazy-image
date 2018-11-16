3197f48db437354af1ccab3a433ab50a682936ba
Arm Supakorn <rainstorm.armiie@gmail.com>
2018-11-16 17:28:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import img from './img.jpg'
+import img3 from './img3.jpg'
+import './App.css'
+import styled from 'styled-components'
+import ProgressiveImage from 'react-progressive-image'
+import LazyImage from 'react-lazy-progressive-image'
+
+const Image = styled.img`
+  transition: all 0.25s ease;
+  /* opacity: ${props => (props.loading ? 0.2 : 1)}; */
+  filter: ${props => (props.loading ? 'blur(10px)' : 'none')};
+`
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p>Test Image Placeholder</p>
+        <ProgressiveImage delay={300} src={img} placeholder={img3}>
+          {(src, loading) => (
+            <img
+              style={{ filter: loading ? 'blur(10px)' : 'none', transition: 'all .1s ease-in' }}
+              width="1000"
+              src={src}
+              alt=""
+            />
+          )}
+        </ProgressiveImage>
+        <LazyImage placeholder={img3} src={img}>
+          {(src, loading) => <Image alt="" width="1000" src={src} loading={loading} />}
+        </LazyImage>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
